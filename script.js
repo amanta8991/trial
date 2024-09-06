65a1d1fac0d0df6250e1f63645f60f8c5778558b
amanta baby <amanta8991@gmail.com>
2024-09-07 03:01:27 +0530

    f

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 const preloader = document.querySelector("[data-preload]");
 
-// window.addEventListener("load", function () {
-//   preloader.classList.add("loaded");
-//   document.body.classList.add("loaded");
-// });
-
-window.addEventListener('load', function () {
-  preloader.classList.add('loaded');
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
 });
 
 
